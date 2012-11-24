764e2262241eb4a4454f2859fd5b2c9d49745cb8
Anne <Anne@Anne-PC>
2012-11-24 23:47:48 +0200

    delimitator, cuv cheie, operator in array functions

--- src/Atom/Delimitator.java
+++ src/Atom/Delimitator.java
@@ -5,6 +5,16 @@ package Atom;
  *
  * @author Miki
  */
-public class Delimitator {
+public class Delimitator extends Atom{
     String delimitator[] = {";",",",".",":",".."};
+    
+    public boolean isDelimitator(String s){
+        for (String d : delimitator) {
+            if(d.equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
